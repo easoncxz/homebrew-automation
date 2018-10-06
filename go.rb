32320a16c4c4ba664b66e97594eb9d7c12ba4364
Eason Chen <easoncxz@gmail.com>
2018-10-06 18:10:13 +1300

    Add workflow usage example

---
+++ go.rb
@@ -0,0 +1,12 @@
+
+require_relative './lib/homebrew_automation.rb'
+
+def go
+  include HomebrewAutomation
+  workflow = Workflow.new(
+    Tap.new('easoncxz', 'homebrew-tap', ENV['EASONCXZ_GITHUB_OAUTH_TOKEN'], keep_submodule: true),
+    Bintray.new('easoncxz', ENV['EASONCXZ_BINTRAY_API_KEY']))
+  workflow.build_and_upload_bottle(
+    SourceDist.new('easoncxz', 'hack-assembler', 'v0.1.1.17'))
+  workflow
+end
