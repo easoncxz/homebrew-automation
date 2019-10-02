c3ff11302fac872d627d74318ba29bd33bce8476
Eason Chen <easoncxz@gmail.com>
2019-10-02 16:35:15 +1300

    Add a `Maybe` effect, with tests

--- lib/homebrew_automation/effects/maybe.rb
+++ lib/homebrew_automation/effects/maybe.rb
@@ -3,6 +3,28 @@ module HomebrewAutomation::Effects
 
   class Maybe < Eff
 
+    def self.pure(x)
+      Maybe.new do
+        x
+      end
+    end
+
+    def self.nothing
+      Maybe.new do
+        nil
+      end
+    end
+
+    def run!
+      current = nil
+      @steps.each do |func_eff|
+        eff = func_eff.call(current)
+        current = eff.run!
+        return nil if current.nil?
+      end
+      current
+    end
+
   end
 
 end
