bb6b310c5c599e81058c0c1321558fbcb38eae23
Eason Chen <easoncxz@gmail.com>
2019-10-03 19:11:59 +1300

    Add docs for all remaining Eff subclasses

--- lib/homebrew_automation/effects/many.rb
+++ lib/homebrew_automation/effects/many.rb
@@ -1,18 +1,57 @@
 
 module HomebrewAutomation::Effects
 
+  # A representation of a value that has many possibilities
+  #
+  # Binding together two +Many+ actions means to consider
+  # all the possibilities (elements) by taking elements pair-wise from
+  # those two actions, in self-major order.
+  #
+  # By "self-major order", consider this example:
+  #
+  #     pairs = xs.bind do |x|
+  #       ys.bind do |y|
+  #         Many.pure([x, y])
+#         end
+  #     end
+  #
+  # In the above, +pairs+ will be in the order of:
+  #
+  #     [ [x_1, y_1]
+  #     , [x_1, y_2]
+  #     , ...
+  #     , [x_1, y_n]
+  #     , [x_2, y_1]
+  #     , ...
+  #     , [x_2, y_n]
+  #     , ...
+  #     , [x_m, y_n]
+  #     ]
+  #
+  # Consider this a generic type with one parametre,
+  # i.e. +Many<a>+.
+
   class Many < Eff
 
+    # Just one possibility
+    #
+    # @param x [a] that one possibility
+    # @return [Many<a>]
     def self.pure(x)
       Many.new do
         [x]
       end
     end
 
+    # Just the possibilities as given in the array
+    #
+    # @param x [Array<a>] the list of all possibilities
+    # @return [Many<a>]
     def self.from_array(xs)
       Many.new do xs end
     end
 
+    # @return [Array[a]] all the possibilities listed out as an +Array+
     def run!
       first, *rest = @steps   # Ruby creates copies, not references
       if rest.empty?
--- lib/homebrew_automation/effects/state.rb
+++ lib/homebrew_automation/effects/state.rb
@@ -1,11 +1,28 @@
 
 module HomebrewAutomation::Effects
 
-  # An isomorphic type to +Block(s -> Tuple[a, s])+
+  # A +State<s, a>+ object represents a pure value of type +a+ with some
+  # potential effect on an implicit state of type +s+
   #
-  # Consider this a generic type with two type parameters, i.e. +State<s, a>+.
+  # Because a State object represents an *effect* on a state, it doesn't
+  # represent any particular state, but rather a *change* of state.  I.e., a
+  # function, or a block of code, that operates on a state to return a new
+  # state. To interact with this implicit state (explicitly), you must use
+  # +::get+, +::put+, or anything built on top of them, e.g. +::modify+.
+  #
+  # To +#bind+ together two State effects, is to create a bigger effect that
+  # performs those two given stateful effects one after the other, while
+  # passing the +a+ value from the first State to the second one, via the
+  # argument to the block from which the second State is returned.
+  #
+  # +State<s, a>+ is isomorphic to +Block(s -> Tuple[a, s])+ in the following
+  # way:
+  #
+  # * +::new+ builds a +State+ object from a Block;
+  # * +#run!+ on a +State+ is like +#call+ on that Block.
   class State < Eff
 
+    # Turning a Block back into a +State+-like object (with a +#run!+)
     class BaseState
 
       # @yieldparam state [s]
@@ -20,21 +37,39 @@ module HomebrewAutomation::Effects
 
     end
 
-    # Different format from other Eff subclasses.
+    # Create a +State+ effect from a Block
+    #
+    # This is nice for pretending youre writing +State+ literals.
+    #
+    # Here's a +State<Integer, String>+ object that represents the value +'foo'+
+    # and adds one to implicit state (of type +Integer+):
+    #
+    #     State.new do |s|
+    #       ['foo', s + 1]
+    #     end
     #
     # @yieldparam s [s]
     # @yieldreturn [Tuple[a, s]]
-    # @return [State<a>]
+    # @return [State<s, a>]
     def initialize(&step)
       @steps = [->(_) { BaseState.new(&step) }]
     end
 
+    # Just a value, with no effect on the implicit state
+    #
+    # @param x [a] the value
+    # @return [forall t. State<t, a>]
     def self.pure(x)
       State.new do |s|
         [x, s]
       end
     end
 
+    # Run this effects to get a value and a new state
+    #
+    # This passes the value along the sequence of actions that happen,
+    # and threads the implicit state through the actions one by one.
+    #
     # @param s [s] initial state
     # @return [Tuple[a, s]] a pair containing a value of type +a+ and
     #   the final state of type +s+
@@ -46,28 +81,44 @@ module HomebrewAutomation::Effects
       [x, s]
     end
 
-    def self._block_not_expected_msg
-      "Block not expected here. Did you forget to call #bind, #apply, #map, or something?"
-    end
-
+    # Read the implicit state as an explicit value
+    #
+    # This just reads the state, but makes no changes to it.
+    #
+    # @return [forall t. State<t, t>]
     def self.get
       if block_given?
-        raise StandardError.new self._block_not_expected_msg
+        raise StandardError.new "Block not expected here. Did you forget to call #bind, #apply, #map, or something?"
       end
       State.new do |s|
         [s, s]
       end
     end
 
+    # Overwrite the implicit state, leaving a nil value
+    #
+    # This ignores what the state previously was, and uses the argument as the
+    # new state. The value is filled out to be nil.
+    #
+    # @param x [t]
+    # @return [State<NilClass, t>]
     def self.put(x)
       if block_given?
-        raise StandardError.new self._block_not_expected_msg
+        raise StandardError.new "Block not expected here. Did you forget to call #bind, #apply, #map, or something?"
       end
       State.new do |_|
         [nil, x]
       end
     end
 
+    # Change the implicit state via a Block
+    #
+    # This is semantically no different from firsting doing +State.get+
+    # and then doing +State.put+.
+    #
+    # @yieldparam s [s] previous state
+    # @yieldreturn [s] new state you want
+    # @return [State<NilClass, s>]
     def self.modify(&on_state)
       State.new do |s|
         [nil, on_state.call(s)]
