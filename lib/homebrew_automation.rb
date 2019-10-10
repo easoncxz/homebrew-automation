cb946ab50ce50e401d862b5ae950880060ce7e3a
Eason Chen <easoncxz@gmail.com>
2019-10-10 23:24:18 +1300

    Delete code about reified effects

--- lib/homebrew_automation.rb
+++ lib/homebrew_automation.rb
@@ -3,7 +3,6 @@
 
 require_relative 'homebrew_automation/bintray.rb'
 require_relative 'homebrew_automation/bottle.rb'
-require_relative 'homebrew_automation/effects.rb'
 require_relative 'homebrew_automation/formula.rb'
 require_relative 'homebrew_automation/git.rb'
 require_relative 'homebrew_automation/mac_os.rb'
--- lib/homebrew_automation/effect_providers.rb
+++
@@ -1,8 +0,0 @@
-
-module HomebrewAutomation
-  module EffectProviders
-  end
-end
-
-require_relative './effect_providers/brew.rb'
-require_relative './effect_providers/file.rb'
